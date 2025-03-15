3220d107f07ce1c102da292f290f5c2c01d269d0
slimyzockt77 <slimyzockt77@gmail.com>
2025-03-15 19:20:15 +0100

    63

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,7 @@ in
   boot.loader.efi.canTouchEfiVariables = true;
   networking.hostName = "nixos"; # Define your hostname.
 
-  # Configure network proxy if necessary
+  # Configure network proxy if necessary.
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
