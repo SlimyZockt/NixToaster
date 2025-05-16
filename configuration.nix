a5c581ff094c3744c323badd807dd7e7d2afcfb6
SlimyZockt <slimyzockt77@gmail.com>
2025-05-16 17:11:26 +0200

    144

--- configuration.nix
+++ configuration.nix
@@ -71,7 +71,7 @@ in
 
   # Bootloader.
   # boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
