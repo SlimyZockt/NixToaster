5fc5a8221ed992dc9179b8eafe7770c3996d8f26
SlimyZockt <slimyzockt77@gmail.com>
2025-06-22 17:52:03 +0200

    216

--- configuration.nix
+++ configuration.nix
@@ -417,7 +417,6 @@ in
   # };
 
   security.polkit.enable = true;
-  security.polkit.package = pkgs.hyprpolkitagent;
 
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   boot.kernelModules = [ "v4l2loopback" ];
