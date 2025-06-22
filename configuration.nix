c5b0f5be81974a79495220ff36a2055ea58a8fe1
SlimyZockt <slimyzockt77@gmail.com>
2025-06-22 21:13:55 +0200

    225

--- configuration.nix
+++ configuration.nix
@@ -414,6 +414,22 @@ in
   #   enableSSHSupport = true;
   # };
 
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
   security.polkit.enable = true;
   security.pam.services.gdm.enableGnomeKeyring = true;
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
