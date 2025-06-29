627791dbc6b4eb66bba6bee9e09495fc2ac19ce9
SlimyZockt <slimyzockt77@gmail.com>
2025-06-29 18:42:06 +0200

    239

--- configuration.nix
+++ configuration.nix
@@ -414,6 +414,7 @@ in
   # };
 
   systemd = {
+    coredump.enable = true;
     user.services.polkit-gnome-authentication-agent-1 = {
       description = "polkit-gnome-authentication-agent-1";
       wantedBy = [ "graphical-session.target" ];
