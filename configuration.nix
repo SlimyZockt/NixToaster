0767a3821073d4086c760b3e1169b49cbfc3e113
SlimyCode <slimyzockt77@gmail.com>
2025-07-15 11:38:29 +0200

    260

--- configuration.nix
+++ configuration.nix
@@ -6,10 +6,8 @@
   config,
   pkgs,
   inputs,
-  lib,
   ...
 }:
-with lib;
 let
   progLangauge = with pkgs; [
     bun
@@ -71,54 +69,6 @@ let
     vscode-langservers-extracted
   ];
 
-  build-dependent-pkgs = with pkgs; [
-    acl
-    attr
-    bzip2
-    curl
-    libsodium
-    libssh
-    libxml2
-    openssl
-    stdenv.cc.cc
-    systemd
-    util-linux
-    xz
-    zlib
-    zstd
-    glib
-    libcxx
-  ];
-  makePkgConfigPath = x: makeSearchPathOutput "dev" "lib/pkgconfig" x;
-  makeIncludePath = x: makeSearchPathOutput "dev" "include" x;
-
-  nvim-depends-library = pkgs.buildEnv {
-    name = "nvim-depends-library";
-    paths = map lib.getLib build-dependent-pkgs;
-    extraPrefix = "/lib/nvim-depends";
-    pathsToLink = [ "/lib" ];
-    ignoreCollisions = true;
-  };
-  nvim-depends-include = pkgs.buildEnv {
-    name = "nvim-depends-include";
-    paths = splitString ":" (makeIncludePath build-dependent-pkgs);
-    extraPrefix = "/lib/nvim-depends/include";
-    ignoreCollisions = true;
-  };
-  nvim-depends-pkgconfig = pkgs.buildEnv {
-    name = "nvim-depends-pkgconfig";
-    paths = splitString ":" (makePkgConfigPath build-dependent-pkgs);
-    extraPrefix = "/lib/nvim-depends/pkgconfig";
-    ignoreCollisions = true;
-  };
-  buildEnv = [
-    "CPATH=${config.home.profileDirectory}/lib/nvim-depends/include"
-    "CPLUS_INCLUDE_PATH=${config.home.profileDirectory}/lib/nvim-depends/include/c++/v1"
-    "LD_LIBRARY_PATH=${config.home.profileDirectory}/lib/nvim-depends/lib"
-    "LIBRARY_PATH=${config.home.profileDirectory}/lib/nvim-depends/lib"
-    "NIX_LD_LIBRARY_PATH=${config.home.profileDirectory}/lib/nvim-depends/lib"
-    "PKG_CONFIG_PATH=${config.home.profileDirectory}/lib/nvim-depends/pkgconfig"
-  ];
 in
 {
   imports = [
@@ -261,11 +211,6 @@ in
     ];
     packages = with pkgs; [
       iwgtk
-      patchelf
-      nvim-depends-include
-      nvim-depends-library
-      nvim-depends-pkgconfig
-      ripgrep
       clinfo
       wine
       networkmanagerapplet
@@ -346,26 +291,6 @@ in
       defaultEditor = true;
       viAlias = true;
       vimAlias = true;
-
-      package = pkgs.neovim;
-
-      withNodeJs = true;
-      withPython3 = true;
-      withRuby = true;
-
-      extraPackages = with pkgs; [
-        doq
-        sqlite
-        cargo
-        clang
-        cmake
-        gcc
-        gnumake
-        ninja
-        pkg-config
-        yarn
-      ];
-
     };
 
     git = {
